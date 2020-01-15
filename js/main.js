a0e1556501c2d8d8a7f9b639a1e7e0648b8be636
ViacheslavSheviakov <viacheslav.sheviakov@nure.ua>
2020-01-15 19:16:02 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    firstName: "Bob",
+    lastName: "Smith",
+    items: [{
+        id: 1,
+        title: "potato",
+        price: 22.2
+      },
+      {
+        id: 2,
+        title: "tomato",
+        price: 25
+      },
+      {
+        id: 3,
+        title: "cheese",
+        price: 15
+      },
+      {
+        id: 4,
+        title: "onion",
+        price: 10
+      }
+    ]
+  },
+  // methods: {},
+});
